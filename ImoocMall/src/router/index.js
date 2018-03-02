873988162fb132b7d9d8af906487f50451767c7e
wikihong <hjpChinash@163.com>
2018-03-02 17:38:33 +0800

    0302-动态路由

--- ImoocMall/src/router/index.js
+++ ImoocMall/src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Goodslist from './../views/Goodslist.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/goods/:goodsId',
+      name: 'Goodslist',
+      component: Goodslist
     }
   ]
 })
---
+++ ImoocMall/src/views/Goodslist.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    这个是商品列表页面
+    <span>{{ $route.params.goodsId }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
