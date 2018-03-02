527eec8f9b4dfc449e9b655b74aa6bdaf8d6bac0
wikihong <hjpChinash@163.com>
2018-03-02 16:38:54 +0800

    0302-组件通信1

---
+++ ImoocMall/src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <p><span>{{ num }}</span></p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      num: 0
+    }
+  },
+  methods: {
+    increment () {
+      console.log(12)
+      this.num++
+    },
+    decrement () {
+      console.log(23)
+      this.num--
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
