5c948760e80a07e8f6635f957cfd939056520f9c
wikihong <hjpChinash@163.com>
2018-03-06 16:17:17 +0800

    0306-图片懒加载

--- ImoocMall/src/main.js
+++ ImoocMall/src/main.js
@@ -3,12 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueLazyLoad from 'vue-lazyload'
 // import {sum, minus} from './util.js'
 // import * as util from './util.js'
 
 // console.log(`sum:${util.sum(3, 5)}, minus:${util.minus(10, 3)}`)
 Vue.config.productionTip = false
 
+Vue.use(VueLazyLoad, {
+  loading: "../static/loading-svg/loading-bars.svg"
+})
 /* eslint-disable no-new */
 new Vue({
   // el: '#app',
--- ImoocMall/src/views/Goodslist.vue
+++ ImoocMall/src/views/Goodslist.vue
@@ -10,25 +10,16 @@
             <span class="sortby">Sort by:</span>
             <a href="javascript:void(0)" class="default cur">Default</a>
             <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
-            <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+            <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">Filter by</a>
           </div>
           <div class="accessory-result">
             <!-- filter -->
-            <div class="filter stopPop" id="filter">
+            <div class="filter stopPop" id="filter" :class="{'filterby-show': filterBy}">
               <dl class="filter-price">
                 <dt>Price:</dt>
-                <dd><a href="javascript:void(0)">All</a></dd>
-                <dd>
-                  <a href="javascript:void(0)">0 - 100</a>
-                </dd>
-                <dd>
-                  <a href="javascript:void(0)">100 - 500</a>
-                </dd>
-                <dd>
-                  <a href="javascript:void(0)">500 - 1000</a>
-                </dd>
-                <dd>
-                  <a href="javascript:void(0)">1000 - 2000</a>
+                <dd><a href="javascript:void(0)" :class="{'cur': priceChecked == 'all'}" @click="priceChecked = all">All</a></dd>
+                <dd v-for="(price,index) in priceFilter" >
+                  <a href="javascript:void(0)" @click="setPriceFilter(index)" :class="{'cur':priceChecked == index}">{{ price.startPrice }} - {{ price.endPrice }}</a>
                 </dd>
               </dl>
             </div>
@@ -39,7 +30,7 @@
                 <ul>
                   <li v-for="item in goodsList">
                     <div class="pic">
-                      <a href="#"><img :src="'/static/'+item.productImg" alt=""></a>
+                      <a href="#"><img v-lazy="'/static/'+item.productImg" alt=""></a>
                     </div>
                     <div class="main">
                       <div class="name">{{ item.productName }}</div>
@@ -55,6 +46,7 @@
           </div>
         </div>
       </div>
+      <div class="md-overlay" v-show="overLayFlag" @click="closePop"></div>
       <nav-footer></nav-footer>
     </div>
 </template>
@@ -73,7 +65,24 @@ export default{
   },
   data () {
     return {
-      goodsList: []
+      goodsList: [],
+      priceFilter: [
+        {
+          startPrice: '0.00',
+          endPrice: '500.00'
+        },
+        {
+          startPrice: '500.00',
+          endPrice: '1000.00'
+        },
+        {
+          startPrice: '1000.00',
+          endPrice: '1500.00'
+        }
+      ],
+      priceChecked: true,
+      filterBy: false,
+      overLayFlag: false
     }
   },
   mounted: function () {
@@ -85,6 +94,20 @@ export default{
         console.log(res.data)
         this.goodsList = res.data.result
       })
+    },
+    // 响应式布局价格的显示隐藏
+    showFilterPop: function () {
+      this.filterBy = true
+      this.overLayFlag = true
+    },
+    closePop: function () {
+      this.filterBy = false
+      this.overLayFlag = false
+    },
+    setPriceFilter: function (index) {
+      this.priceChecked = index
+      this.filterBy = false
+      this.overLayFlag = false
     }
   }
 }
